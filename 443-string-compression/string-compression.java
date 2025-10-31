a8eae7244d1fc717305788a93bdde05022d4366a
Divyansh Garg <divyanshgarg844@gmail.com>
2025-10-31 23:02:19 +0530

    Time: 1 ms (99.66%) | Memory: 45.3 MB (5.34%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int compress(char[] chars) {
+        int index = 0; // position to write in chars[]
+        int i = 0;
+        
+        while (i < chars.length) {
+            char current = chars[i];
+            int count = 0;
+            
+            
+            while (i < chars.length && chars[i] == current) {
+                count++;
+                i++;
+            }
+            
+            
+            chars[index++] = current;
+            
+            
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+        
+        return index; 
+    }
+}
