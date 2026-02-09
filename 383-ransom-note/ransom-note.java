7b8d4329a0008e2eed2f8741042b3fd7dde74c17
Divyansh Garg <divyanshgarg844@gmail.com>
2026-02-09 22:38:26 +0530

    Time: 16 ms (20.52%) | Memory: 47.4 MB (12.60%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<magazine.length();i++){
+           map.put(magazine.charAt(i),map.getOrDefault(magazine.charAt(i),0)+1);
+        }
+        int count =0;
+        for(int i=0;i<ransomNote.length();i++){
+            char ch = ransomNote.charAt(i);
+            if(map.containsKey(ch) && map.get(ch)>0){
+                count++;
+                map.put(ch,map.get(ch) - 1);
+            }
+            else{
+                break;
+            }
+        }
+        if(count == ransomNote.length()){
+            return true;
+        }
+        return false;
+    }
+}
