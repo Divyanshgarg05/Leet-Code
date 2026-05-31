1f4b57db6023021e9552b914a40a4debb6c01962
Divyansh Garg <divyanshgarg844@gmail.com>
2026-05-31 22:38:59 +0530

    Time: 48 ms (19.21%) | Memory: 97.2 MB (5.17%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+      Map<Integer,Integer> map = new HashMap<>();
+      int min = nums[0] , max = nums[0];
+
+      for(int i=0;i<nums.length;i++){
+        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+
+        min = Math.min(min,nums[i]);
+        max = Math.max(max,nums[i]);
+
+      }
+      int index=0;
+      for(int i=min;i<=max;i++){
+        while(map.getOrDefault(i,0) > 0){
+            nums[index] = i;
+            index++;
+            map.put(i,map.get(i) - 1);
+        }
+      }
+      return nums;
+    }
+}
+
+
+ 
