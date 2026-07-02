14557ea7ddb645b572708491de1b6dca3618f92b
Divyansh Garg <divyanshgarg844@gmail.com>
2026-07-02 22:05:01 +0530

    Time: 128 ms (88.61%) | Memory: 42.5 MB (97.22%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    public boolean exist(char[][] board, String word) {
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < cols; j++) {
+
+                if (dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean dfs(char[][] board, String word,
+                       int row, int col, int index) {
+
+        // Word found
+        if (index == word.length())
+            return true;
+
+        // Invalid position
+        if (row < 0 || col < 0 ||
+            row >= board.length ||
+            col >= board[0].length ||
+            board[row][col] != word.charAt(index))
+            return false;
+
+        // Mark visited
+        char temp = board[row][col];
+        board[row][col] = '#';
+
+        boolean found =
+                dfs(board, word, row + 1, col, index + 1) ||
+                dfs(board, word, row - 1, col, index + 1) ||
+                dfs(board, word, row, col + 1, index + 1) ||
+                dfs(board, word, row, col - 1, index + 1);
+
+        // Backtrack
+        board[row][col] = temp;
+
+        return found;
+    }
+}
