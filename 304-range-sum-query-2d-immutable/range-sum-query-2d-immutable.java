d7d0cc05b66de787bd273df7c38749dd8bb6e732
Divyansh Garg <divyanshgarg844@gmail.com>
2026-08-11 10:50:42 +0530

    Time: 106 ms (40.73%) | Memory: 141.9 MB (64.98%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,98 +1,53 @@
 class NumMatrix {
 
-    // Original matrix (optional, not actually needed after preprocessing)
+    int[][] pSum;
     int[][] matrix;
 
-    // Prefix Sum Matrix
-    // pmSum[i][j] = Sum of all elements from (0,0) to (i,j)
-    int[][] pmSum;
-
-    // Builds the Prefix Sum Matrix
-    public void getMatrixPrefixSum(int[][] matrix) {
-
-        // First cell
-        pmSum[0][0] = matrix[0][0];
-
-        // Fill first row
-        // Since there is nothing above, keep adding from left
-        for (int col = 1; col < matrix[0].length; col++) {
-            pmSum[0][col] = pmSum[0][col - 1] + matrix[0][col];
+    public void getPSum(int[][] matrix){
+        pSum[0][0] = matrix[0][0];
+        for(int i=1;i<matrix[0].length;i++){
+            pSum[0][i] = pSum[0][i-1] + matrix[0][i];
+        }
+        for(int j=1;j<matrix.length;j++){
+            pSum[j][0] = pSum[j-1][0] + matrix[j][0];
         }
 
-        // Fill first column
-        // Since there is nothing on the left, keep adding from top
-        for (int row = 1; row < matrix.length; row++) {
-            pmSum[row][0] = pmSum[row - 1][0] + matrix[row][0];
-        }
-
-        // Fill the remaining cells
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 1; j < matrix[0].length; j++) {
-
-                /*
-                 * Formula:
-                 *
-                 * Top Prefix
-                 * + Left Prefix
-                 * - Top Left Prefix (because it gets counted twice)
-                 * + Current Cell
-                 */
-
-                pmSum[i][j] =
-                        pmSum[i - 1][j]
-                      + pmSum[i][j - 1]
-                      - pmSum[i - 1][j - 1]
-                      + matrix[i][j];
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
+                pSum[i][j] = pSum[i-1][j] + pSum[i][j-1] - pSum[i-1][j-1] + matrix[i][j];
             }
         }
     }
 
-    // Returns prefix sum safely
-    // If row or column becomes negative,
-    // return 0 (helps while calculating boundaries)
-    public int getValue(int row, int col) {
-
-        if (row < 0 || col < 0)
+    public int getValue(int row,int col){
+        if(row<0 || col<0){
             return 0;
-
-        return pmSum[row][col];
+        }
+        return pSum[row][col];
     }
 
     public NumMatrix(int[][] matrix) {
-
         this.matrix = matrix;
-
-        pmSum = new int[matrix.length][matrix[0].length];
-
-        getMatrixPrefixSum(matrix);
+        pSum = new int[matrix.length][matrix[0].length];
+        getPSum(matrix);
     }
-
+    
     public int sumRegion(int row1, int col1, int row2, int col2) {
+        //Total Sum
+        int total = getValue(row2,col2);
+        //top
+        int top = getValue(row1-1,col2);
 
-        /*
-         * total = Entire rectangle till Bottom Right
-         */
-        int total = getValue(row2, col2);
+        //left
+        int left = getValue(row2,col1-1);
 
-        /*
-         * Remove upper row
-         */
-        int top = getValue(row1 - 1, col2);
-
-        /*
-         * Remove left col
-         */
-        int left = getValue(row2, col1 - 1);
-
-        /*
-         * Add overlap back
-         * (because it was removed twice)
-         */
-        int overlap = getValue(row1 - 1, col1 - 1);
+        //overlap
+        int overlap = getValue(row1-1,col1-1);
 
         return total - top - left + overlap;
     }
 }
+
 /**
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix obj = new NumMatrix(matrix);
